555fa9573724702899883c5237ceb725407582f1
samakshmittal <samakshmittal826@gmail.com>
2023-04-26 22:23:50 +0530

    Create 4.py

---
+++ Lab Manual/Practical 2/4.py
@@ -0,0 +1,4 @@
+from math import sqrt
+x=int(input("Enter the value of perpendicular of a right angled triangle"))
+y=int(input("Enter the value of base of a right angled triangle"))
+print("Hypotenuse is =", sqrt(x*x+y*y))
