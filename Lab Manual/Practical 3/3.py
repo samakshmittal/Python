650de2db2b9761299717fd2983ada1550a0cb2b6
samakshmittal <samakshmittal826@gmail.com>
2023-04-27 22:40:08 +0530

    Create 3.py

---
+++ Lab Manual/Practical 3/3.py
@@ -0,0 +1,8 @@
+a=int(input("Enter 1st number"))
+b=int(input("Enter 2nd number"))
+if(a>b):
+    print(a, "is greater than", b)
+elif(a<b):
+    print(b, "is greater than", a)
+else:
+    print(a, "is equal to", b)
