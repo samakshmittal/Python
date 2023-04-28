1ab8b92c474b2d8be49451fbbae5bd8c6a28a56b
samakshmittal <samakshmittal826@gmail.com>
2023-04-28 12:02:20 +0530

    Create 8.py

---
+++ Lab Manual/Practical 5/8.py
@@ -0,0 +1,9 @@
+s1={"Red", "Yellow", "Orange", "Blue"}
+s2={"Violet", "Blue", "Purple"}
+print("Intersection", s1.intersection(s2))
+print("Difference", s1.difference(s2))
+print("Union", s1.union(s2))
+s1.add("Pink")
+print("After Adding s1:", s1)
+print("Count of set 1 :", len(s1))
+print("Count of set 2 :", len(s2))
