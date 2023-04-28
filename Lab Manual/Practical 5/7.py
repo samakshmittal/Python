288a8d59e461af376ead50790292afdbd6bf544a
samakshmittal <samakshmittal826@gmail.com>
2023-04-28 12:02:23 +0530

    Create 7.py

---
+++ Lab Manual/Practical 5/7.py
@@ -0,0 +1,6 @@
+s1=set(input("Enter fruits in set 1"))
+s2=set(input("Enter fruits in set 2"))
+print(s1.intersection(s2))
+print(s1.difference(s2))
+print("Count of set 1 :", len(s1)-1)
+print("Count of set 2 :", len(s2)-1)
